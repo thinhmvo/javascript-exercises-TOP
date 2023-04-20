02e420b911f7c85be3397fc02594433b57ca5608
Thinh Mike Vo <thinhmvo101@users.noreply.github.com>
2023-04-19 21:00:14 -0700

    completed exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reverseInput = '';
+    for (let i = input.length -1    ; i >= 0; i--) {
+        reverseInput += input[i];
+    }
+    return reverseInput;
 };
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
