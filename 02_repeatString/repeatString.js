b860424d629d7f57a2646e5e640faf5f4de96e43
Thinh Mike Vo <thinhmvo101@users.noreply.github.com>
2023-04-19 20:27:31 -0700

    completed repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (input, amount) {
+  let userStatement = "";
+  if (amount < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < amount; i++) {
+      userStatement += input;
+    }
+    return userStatement;
+  }
 };
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
