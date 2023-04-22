75ec9d8abfdae822c27fc72897a2cacceed29c62
Thinh Mike Vo <thinhmvo101@users.noreply.github.com>
2023-04-21 19:06:49 -0700

    temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputNum) {
+  let outputNum = (inputNum - 32) * 5/9; 
+  return parseFloat(outputNum.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputNum) {
+  let outputNum = (inputNum * 9/5 + 32); 
+  return parseFloat(outputNum.toFixed(1));
 };
 
 // Do not edit below this line
