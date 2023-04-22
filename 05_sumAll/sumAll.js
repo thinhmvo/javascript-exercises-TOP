d67976ba79ba85f152c03e4f407afbfe015bda21
Thinh Mike Vo <thinhmvo101@users.noreply.github.com>
2023-04-21 18:41:05 -0700

    Completed 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let totalSum = 0;
+    let startNum = 0;
+    let endNum = 0;
+    if (typeof(start) !== "number" || typeof(end) !== "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if(start > end) {
+        endNum =  start;
+        startNum = end;
+    } else {
+        startNum = start;
+        endNum = end;
+    }
+    
+    for (let i = startNum; i < endNum + 1; i++) {
+        totalSum += i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
